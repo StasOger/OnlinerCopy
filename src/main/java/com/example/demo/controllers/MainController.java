81a3b3b706d608448b1aaae44441b0ce0df29be1
StasOger <92465858+StasOger@users.noreply.github.com>
2022-11-28 00:26:19 +0300

    login, registration

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,20 +1,48 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Card;
+import com.example.demo.model.Cvc;
+import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import role.User;
+
+import java.util.List;
 
 
 @Controller
 public class MainController {
 
+    static final Logger log = LoggerFactory.getLogger(MainController.class);
 
+    private UserRepository userRepository = new UserRepository();
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "have we go again");
-        return "home";
+        return "/";
     }
+
+    @PostMapping("/loginUser")
+    public String loginUser(@ModelAttribute User user)
+    {
+        List<User> userList = userRepository.getAllUsers();
+
+        log.error(user.toString());
+
+        for (User u: userList){
+            if (u.getEmail().equalsIgnoreCase(user.getEmail()) && u.getPassword().equalsIgnoreCase(user.getPassword())){
+                return "redirect:/Cars";
+            }
+        }
+        return "redirect:/PROFILE";
+    }
+
     @GetMapping("/support")
     public String support(Model model) {
         model.addAttribute("title", "have we go again");
---
+++ src/main/java/role/User.java
@@ -0,0 +1,77 @@
+package role;
+
+public class User {
+
+    private int id;
+    private String role;
+    private String login;
+    private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(int id, String role, String login, String password, String email) {
+        this.id = id;
+        this.role = role;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
