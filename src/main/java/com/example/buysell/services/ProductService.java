083726c54075d1bdc3c946bb0ad88817cae1958f
StasOger <92465858+StasOger@users.noreply.github.com>
2023-01-12 01:52:31 +0300

    BD postgresql+photo

--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -1,13 +1,18 @@
 package com.example.buysell.services;
 
+import com.example.buysell.models.Image;
 import com.example.buysell.models.Product;
 import com.example.buysell.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.hibernate.loader.internal.AliasConstantsHelper.get;
 
 @Service
 @Slf4j
@@ -20,16 +25,45 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product){
-        log.info("Saving new {}", product);
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+
+        log.info("Saving new Product. Title: {}; Author: {}", product);
+        Product productFromDb = productRepository.save(product);
+        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
     }
 
-    public void deleteProducts(Long id){
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
+    }
+
+    public void deleteProducts(Long id) {
         productRepository.deleteById(id);
     }
 
-    public Product getProductById(Long id){
+    public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
 }
