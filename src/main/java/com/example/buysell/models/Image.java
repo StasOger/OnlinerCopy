8e390da99f129e475450fa811d4a2fd88df6bb9c
StasOger <92465858+StasOger@users.noreply.github.com>
2023-01-12 18:26:10 +0300

    DB + images

--- src/main/java/com/example/buysell/models/Image.java
+++ src/main/java/com/example/buysell/models/Image.java
@@ -3,6 +3,7 @@ package com.example.buysell.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 
@@ -27,6 +28,7 @@ public class Image {
     @Column(name = "isPreviewImage")
     private boolean isPreviewImage;
     @Lob
+//    @Type(type = "org.hibernate.type.ImageType")
     private byte[] bytes;
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private Product product;
